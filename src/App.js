ff96b2c562f6fed59f67c9b00cf9c7edc9ce38d8
juliantfernandez <juliant.fernandez0611@gmail.com>
2022-07-09 17:56:31 -0300

    Landing Page APP hecha con react Terminada

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Header from "./components/Header";
+import Feature  from "./components/Feature";
+import About from "./components/About";
+import Presentation from "./components/Presentation";
+import aboutimage from './images/Frame 19.png'
+import aboutimage1 from './images/download.png'
+import Contact from './components/Contact'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Feature/>
+      <About image={aboutimage} title='Comes With All You Need For Daily Life' button='Get The App'/>
+      <Presentation/>
+      <About image={aboutimage1} title='Download And Get The APP Now' button='Download'/>
+      <Contact/>
     </div>
   );
 }
